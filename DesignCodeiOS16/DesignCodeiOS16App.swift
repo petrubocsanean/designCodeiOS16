369761b513c476da378ae4cd3da8ea90b3493dba
Petru Bocsanean <petru.bocsanean@gmail.com>
2023-02-27 15:12:40 +0100

    Initial Commit

---
+++ DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by Petru Bocsanean on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
